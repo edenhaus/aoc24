55ca043078554126a9320b04ec8724a92bc47c8e
Robert Resch <robert@resch.dev>
2024-12-06 18:02:38 +0000

    ex 6 part 2

--- src/ex6/mod.rs
+++ src/ex6/mod.rs
@@ -1,16 +1,32 @@
 use super::common::Report;
-use std::time::Instant;
+use std::{collections::HashSet, fmt, hash::Hash, time::Instant};
 
 
 const INPUT: &'static str = include_str!("input.txt");
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone)]
 enum Type {
     NotSeen,
-    Seen,
+    SeenUpDown,
+    SeenLeftRight,
     Obstacle,
+    SeenRightTurn,
 }
 
+impl fmt::Debug for Type {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Type::NotSeen => write!(f, "."),
+            Type::SeenLeftRight => write!(f, "-"),
+            Type::SeenUpDown => write!(f, "|"),
+            Type::Obstacle => write!(f, "#"),
+            Type::SeenRightTurn => write!(f, "+")
+        }
+    }
+}
+
+
+#[derive(PartialEq, Eq, Hash, Copy, Clone)]
 enum Direction {
     Up,
     Down,
@@ -18,49 +34,53 @@ enum Direction {
     Right,
 }
 
-pub fn solve(input: &str)-> Report<u32, u32> {
-    let mut map: Vec<Vec<Type>> = Vec::new();
-    let mut y = 0;
-    let mut x = 0;
-    let mut direction = Direction::Up;
-    input.lines().for_each(|line| {
-        let mut new_line: Vec<Type> = Vec::new();
-        line.chars().for_each(|c| {
-            match c {
-                '^' => direction = Direction::Up,
-                'v' => direction = Direction::Down,
-                '<' => direction = Direction::Left,
-                '>' => direction = Direction::Right,
-                '#' => new_line.push(Type::Obstacle),
-                _ => new_line.push(Type::NotSeen),
-            }
-            if c == '^' || c == 'v' || c == '<' || c == '>' {
-                x = new_line.len();
-                y = map.len();
-            }
-        });
-        map.push(new_line);
-    });
+fn next_position(y: usize, x: usize, direction: &Direction) -> (usize, usize) {
+    match direction {
+        Direction::Up => (y-1, x),
+        Direction::Down => (y+1, x),
+        Direction::Left => (y, x-1),
+        Direction::Right => (y, x+1),
+    }
+}
 
-    let mut count_seen:u32 = 0;
-    while y != 0 && y != map.len()-1 && x != 0 && x != map[0].len()-1 {
+fn check_loop(map: &Vec<Vec<Type>>,  y: usize, x: usize,direction: &Direction) -> bool {
+    let mut direction = direction.clone();
+    let mut x = x;
+    let mut y = y;
+    let mut turns: Vec<(usize, usize, Direction)> = Vec::new();
+    let mut map: Vec<Vec<Type>> = map.clone();
+    map[y][x] = Type::Obstacle;
+    turns.push((y, x, direction));
+    // previous position
+    (y,x)= match direction {
+        Direction::Up => (y+1, x),
+        Direction::Down => (y-1, x),
+        Direction::Left => (y, x+1),
+        Direction::Right => (y, x-1),    
+    };
+    let last_y = map.len()-1;
+    let last_x = map[0].len()-1;
+
+    while y != 0 && y != last_y && x != 0 && x != last_x {
+        if turns.contains(&(y, x, direction)) {
+            return true;
+        }
         match map[y][x] {
             Type::NotSeen => {
-                count_seen += 1;
-                map[y][x] = Type::Seen;
+                if direction == Direction::Up || direction == Direction::Down {
+                    map[y][x] = Type::SeenUpDown;
+                } else {
+                    map[y][x] = Type::SeenLeftRight;
+                }
             },
-            Type::Seen => {},
             Type::Obstacle => {
-                panic!("Obstacle at {},{}", x, y);
+                return false;
             },
+            _ =>{}
         }
-        let next = match direction {
-            Direction::Up => (y-1, x),
-            Direction::Down => (y+1, x),
-            Direction::Left => (y, x-1),
-            Direction::Right => (y, x+1),
-        };
+        let next = next_position(y, x, &direction);
         if map[next.0][next.1] == Type::Obstacle {
+            turns.push((y, x, direction));
             match direction {
                 Direction::Up => direction = Direction::Right,
                 Direction::Down => direction = Direction::Left,
@@ -72,8 +92,77 @@ pub fn solve(input: &str)-> Report<u32, u32> {
             x = next.1;
         }
     }
+    false
+}
 
-    Report{exercise:6, first:count_seen+1, second:0}
+pub fn solve(input: &str)-> Report<u32, u32> {
+    let mut map: Vec<Vec<Type>> = Vec::new();
+    let mut y = 0;
+    let mut x = 0;
+    let mut direction = Direction::Up;
+    input.lines().for_each(|line| {
+        let mut new_line: Vec<Type> = Vec::new();
+        line.chars().for_each(|c| {
+            if c == '^' || c == 'v' || c == '<' || c == '>' {
+                x = new_line.len();
+                y = map.len();
+            }
+            match c {
+                '^' => {direction = Direction::Up; new_line.push(Type::SeenUpDown);},
+                'v' => {direction = Direction::Down; new_line.push(Type::SeenUpDown);},
+                '<' => {direction = Direction::Left; new_line.push(Type::SeenLeftRight);},
+                '>' => {direction = Direction::Right; new_line.push(Type::SeenLeftRight);},
+                '#' => new_line.push(Type::Obstacle),
+                _ => new_line.push(Type::NotSeen),
+            }
+        });
+        map.push(new_line);
+    });
+
+    let mut first:u32 = 1;
+    let mut second: HashSet<(usize, usize)> = HashSet::new();
+    (y,x) = next_position(y, x, &direction);
+    while y != 0 && y != map.len()-1 && x != 0 && x != map[0].len()-1 {
+        match map[y][x] {
+            Type::NotSeen => {
+                first += 1;
+                if direction == Direction::Up || direction == Direction::Down {
+                    map[y][x] = Type::SeenUpDown;
+                } else {
+                    map[y][x] = Type::SeenLeftRight;
+                }
+                if first > 0 && check_loop(&map, y, x, &direction){
+                    second.insert((y, x));
+                }
+            },
+            Type::SeenLeftRight => {},
+            Type::SeenUpDown => {},
+            Type::SeenRightTurn => {},
+            Type::Obstacle => {
+                panic!("Obstacle at {},{}", x, y);
+            },
+        }
+        let next = next_position(y, x, &direction);
+        if map[next.0][next.1] == Type::Obstacle {
+            match direction {
+                Direction::Up => direction = Direction::Right,
+                Direction::Down => direction = Direction::Left,
+                Direction::Left => direction = Direction::Up,
+                Direction::Right => direction = Direction::Down,
+            }
+            map[y][x] = Type::SeenRightTurn;
+        } else {
+            y = next.0;
+            x = next.1;
+        }
+    }
+
+    //check putting obstacle at the end
+    if check_loop(&map, y, x, &direction){
+        second.insert((y, x));
+    }
+
+    Report{exercise:6, first:first+1, second:second.len() as u32}
 }
 
 
@@ -103,7 +192,7 @@ mod tests {
 ......#...";
         let report = solve(input);
         assert_eq!(report.first, 41);
-        assert_eq!(report.second, 0);
+        assert_eq!(report.second, 6);
     }
 
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod ex2;
 mod ex3;
 mod ex4;
 mod ex5;
+mod ex6;
 
 fn main() {
     println!("Advent of Code 2024");
@@ -12,4 +13,5 @@ fn main() {
     ex3::run();
     ex4::run();
     ex5::run();
+    ex6::run();
 }
